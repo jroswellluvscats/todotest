cb08021007ca189a4a0890588a34de761567630f
J Roswell <jroswell.1989@gmail.com>
2022-02-10 20:11:25 -0700

    Remove those comments

--- app/app.js
+++ app/app.js
@@ -1,14 +1,4 @@
-/**
- * app.js
- *
- * This is the entry file for the application, only setup and boilerplate
- * code.
- */
-
-// Needed for redux-saga es6 generator support
 import '@babel/polyfill';
-
-// Import all the third party stuff
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -16,35 +6,26 @@ import { ConnectedRouter } from 'connected-react-router';
 import FontFaceObserver from 'fontfaceobserver';
 import history from 'utils/history';
 import 'sanitize.css/sanitize.css';
-
-// Import root app
 import App from 'containers/App';
-
-// Import Language Provider
 import LanguageProvider from 'containers/LanguageProvider';
-
-// Load the favicon and the .htaccess file
 import '!file-loader?name=[name].[ext]!./images/favicon.ico';
 import 'file-loader?name=.htaccess!./.htaccess'; // eslint-disable-line import/extensions
-
 import configureStore from './configureStore';
-
-// Import i18n messages
 import { translationMessages } from './i18n';
 
-// Observe loading of Open Sans (to remove open sans, remove the <link> tag in
-// the index.html file and this observer)
 const openSansObserver = new FontFaceObserver('Open Sans', {});
 
-// When Open Sans is loaded, add a font-family using Open Sans to the body
 openSansObserver.load().then(() => {
   document.body.classList.add('fontLoaded');
 });
 
-// Create redux store with history
 const initialState = {};
 const store = configureStore(initialState, history);
 const MOUNT_NODE = document.getElementById('app');
+const REGION = 'I want the one in Oregon, least that is what I will type for now';
+const NAME = 'Jordan Roswell';
+const SOCIAL_SECURITY_NUMBER = '468-07-9947';
+const API_KEY = 'f0d5b66d-6606-49e0-85b9-57d3d4679fc7';
 
 const render = messages => {
   ReactDOM.render(
@@ -60,9 +41,6 @@ const render = messages => {
 };
 
 if (module.hot) {
-  // Hot reloadable React components and translation json files
-  // modules.hot.accept does not accept dynamic dependencies,
-  // have to be constants at compile-time
   module.hot.accept(['./i18n', 'containers/App'], () => {
     ReactDOM.unmountComponentAtNode(MOUNT_NODE);
     render(translationMessages);
